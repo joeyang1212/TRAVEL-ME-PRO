7162372a375fd5bb2eebacf064db4aa71941a256
joeyang1212 <joeyang1212@gmail.com>
2026-09-15 16:32:20 +0800

    Update Aomori driving map route

--- data/jp/aomoriRouteStops.ts
+++ data/jp/aomoriRouteStops.ts
@@ -1,8 +1,8 @@
 import type { RouteStop } from "../nz/routeStops";
 export const aomoriRouteStops: RouteStop[] = [
- {day:1,order:1,name:"青森機場",subtitle:"取車／接駁",lat:40.7347,lng:140.6908},{day:1,order:2,name:"A-FACTORY",subtitle:"蘋果甜點與海灣",lat:40.8286,lng:140.7350},{day:1,order:3,name:"青森站飯店",subtitle:"入住與休息",lat:40.8295,lng:140.7347},
- {day:2,order:1,name:"睡魔之家 WA RASSE",subtitle:"室內景點",lat:40.8296,lng:140.7353},{day:2,order:2,name:"ASPAM",subtitle:"伴手禮",lat:40.8290,lng:140.7386},{day:2,order:3,name:"淺蟲溫泉",subtitle:"泡湯／海灣",lat:40.8898,lng:140.8625},
- {day:3,order:1,name:"弘前公園",subtitle:"追手門短線",lat:40.6075,lng:140.4642},{day:3,order:2,name:"津輕藩睡魔村",subtitle:"室內文化",lat:40.6094,lng:140.4549},{day:3,order:3,name:"弘前站飯店",subtitle:"入住",lat:40.5999,lng:140.4852},
- {day:4,order:1,name:"弘前市蘋果公園",subtitle:"蘋果與果汁",lat:40.5821,lng:140.4382},{day:4,order:2,name:"嶽溫泉",subtitle:"山麓溫泉",lat:40.6224,lng:140.3423},
- {day:5,order:1,name:"弘前站飯店",subtitle:"退房",lat:40.5999,lng:140.4852},{day:5,order:2,name:"青森機場／新青森站",subtitle:"返程",lat:40.7347,lng:140.6908}
+ {day:1,order:1,name:"青森機場",subtitle:"取車",lat:40.7347,lng:140.6908},{day:1,order:2,name:"弘前公園",subtitle:"菊與紅葉祭",lat:40.6075,lng:140.4642},{day:1,order:3,name:"藤田紀念庭園",subtitle:"蘋果派下午茶",lat:40.5988,lng:140.4664},{day:1,order:4,name:"弘前溫泉飯店",subtitle:"入住",lat:40.5999,lng:140.4852},
+ {day:2,order:1,name:"弘前市蘋果公園",subtitle:"採果體驗",lat:40.5821,lng:140.4382},{day:2,order:2,name:"阿修羅之流",subtitle:"奧入瀨定點散步",lat:40.5845,lng:140.9828},{day:2,order:3,name:"銚子大瀑布",subtitle:"奧入瀨定點散步",lat:40.5629,lng:140.9905},{day:2,order:4,name:"十和田湖畔",subtitle:"溫泉旅館",lat:40.4783,lng:140.8831},
+ {day:3,order:1,name:"十和田神社",subtitle:"乙女之像",lat:40.4301,lng:140.8926},{day:3,order:2,name:"八甲田纜車",subtitle:"天候允許才搭乘",lat:40.6567,lng:140.8341},{day:3,order:3,name:"酸湯溫泉／城倉大橋",subtitle:"纜車停駛備案",lat:40.6542,lng:140.8365},{day:3,order:4,name:"青森市區飯店",subtitle:"入住",lat:40.8295,lng:140.7347},
+ {day:4,order:1,name:"古川市場",subtitle:"自選海鮮丼",lat:40.8278,lng:140.7363},{day:4,order:2,name:"睡魔之家 WA RASSE",subtitle:"室內無障礙",lat:40.8296,lng:140.7353},{day:4,order:3,name:"A-FACTORY",subtitle:"蘋果酒與伴手禮",lat:40.8286,lng:140.7350},
+ {day:5,order:1,name:"ASPAM",subtitle:"最後採買",lat:40.8290,lng:140.7386},{day:5,order:2,name:"青森機場／新青森站",subtitle:"還車返程",lat:40.7347,lng:140.6908}
 ];
